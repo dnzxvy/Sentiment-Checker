7a2deee63fff8f2b5b83e19d27e26032b34d0d7b
dnzxvy <ntukadaniel@gmail.com>
2025-09-17 00:04:54 +0100

    Added IMDB Dataset to include more words to increase accuracy and validity of sentiment

--- sentimentModelTest.py
+++ sentimentModelTest.py
@@ -1,34 +1,63 @@
 import nltk
+import pandas as pd
 from nltk.corpus import movie_reviews
 import random
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+import joblib
+import kagglehub # To download IMDB Dataset
 
 # Download dataset
 nltk.download('movie_reviews')
 
 # Load documents (List of words, labels)
-documents = [(list(movie_reviews.words(fileid)), category)
-             for category in movie_reviews.categories()
-             for fileid in movie_reviews.fileids(category)]
-random.shuffle(documents)
 
-#prepare texts and labels
-texts = [" ".join(words) for words, label in documents]
-labels = [1 if label == 'pos' else 0 for words, label in documents]
+nltk_docs = []
+nltk_labels = []
+
+for category in movie_reviews.categories():
+    for fileid in movie_reviews.fileids(category):
+        nltk_docs.append(movie_reviews.raw(fileid))
+        nltk_labels.append(1 if category == "pos" else 0)
+
+nltk_df = pd.DataFrame({"review": nltk_docs, "label": nltk_labels})
+print("NLTK dataset size:",nltk_df.shape)
+
+path = kagglehub.dataset_download("lakshmi25npathi/imdb-dataset-of-50k-movie-reviews")
+print("Path to dataset files:", path)
+
+csv_path = path + "/IMDB Dataset.csv"
+
+imdb_dataset = pd.read_csv(csv_path)
+print(imdb_dataset.head())
+
+imdb_dataset["label"] = imdb_dataset["sentiment"].apply(lambda x: 1 if x == "positive"
+                                                        else 0)
+imdb_df = imdb_dataset[["review", "label"]]
+print("IMDB dataset size:", imdb_df.shape)
+
+# Combining NLTK + IMDB datasets
+
+df = pd.concat([imdb_df, nltk_df], axis=0).reset_index(drop=True)
+print("Combined dataset size:", df.shape)
+
+# Train and test split
+X_train, X_test, y_train, y_test = train_test_split(
+    df["review"], df["label"], test_size=0.2, random_state=42)
+
+# Vectorize the text ( convert words into numerical features
+#with bag of words)
+
+vectorizer = CountVectorizer(stop_words="english")
 
 
-# Step 2: vectorize the text ( convert words into numerical features
-#with bag of words
-from sklearn.feature_extraction.text import CountVectorizer
-vectorizer = CountVectorizer(max_features=5000)
-X = vectorizer.fit_transform(texts)
 
-# Step 3 train test split. Split training and testing
-from sklearn.model_selection import train_test_split
+
 X_train, X_test, y_train, y_test = train_test_split(
     X, labels, test_size=0.2, random_state=42
 )
 
-from sklearn.naive_bayes import MultinomialNB
 model = MultinomialNB()
 model.fit(X_train, y_train)
 
